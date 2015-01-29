69ba5a8f4906244cec1cee758864def11ca3e107
x00058277 <x00058277@ittd.ie>
2015-01-29 12:43:31 +0000

    Added Demo Class

---
+++ week1/Demo/Class.java
@@ -0,0 +1,20 @@
+package Demo;
+
+public class Class {
+
+	private int size;
+
+	public void getSize() {
+		// TODO - implement Class.getSize
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param size
+	 */
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+}
---
+++ week1/Demo/Example.java
@@ -0,0 +1,39 @@
+package Demo;
+
+public class Example {
+
+	private string name;
+	private int age;
+
+	public Example() {
+		// TODO - implement Example.Example
+		throw new UnsupportedOperationException();
+	}
+
+	public void getName() {
+		// TODO - implement Example.getName
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param name
+	 */
+	public void setName(int name) {
+		// TODO - implement Example.setName
+		throw new UnsupportedOperationException();
+	}
+
+	public int getAge() {
+		return this.age;
+	}
+
+	/**
+	 * 
+	 * @param age
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
